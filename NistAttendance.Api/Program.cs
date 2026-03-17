b30d74d73ebe954d6d2e5819f42784a6a9864a85
Sachin Dsilva <dsilva.sachinronson@nidec.com>
2026-03-17 11:44:13 +0900

    Fix Port issue

--- NistAttendance.Api/Program.cs
+++ NistAttendance.Api/Program.cs
@@ -16,7 +16,8 @@ if (connStr.StartsWith("postgresql://") || connStr.StartsWith("postgres://"))
 {
     var uri = new Uri(connStr);
     var userInfo = uri.UserInfo.Split(':');
-    connStr = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')}"
+    var port = uri.Port > 0 ? uri.Port : 5432;
+    connStr = $"Host={uri.Host};Port={port};Database={uri.AbsolutePath.TrimStart('/')}"
             + $";Username={userInfo[0]};Password={userInfo[1]}"
             + ";SSL Mode=Require;Trust Server Certificate=true";
 }
