84aac7e5d0cb3b05c8ddd652b5f7b71ce07cced0
Sachin Dsilva <dsilva.sachinronson@nidec.com>
2026-04-03 13:52:31 +0900

    Fix endpoint /api/dictionary/labels: 500 Internal Server Error

--- NistAttendance.Api/Migrations/20260403045112_AddDictionary.Designer.cs
+++ NistAttendance.Api/Migrations/20260403045112_AddDictionary.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NistAttendance.Api.Data;
 
@@ -10,9 +11,11 @@ using NistAttendance.Api.Data;
 namespace NistAttendance.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260403045112_AddDictionary")]
+    partial class AddDictionary
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.25");
